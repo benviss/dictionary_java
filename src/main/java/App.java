a8f9230ee1d8fcfd697e63f1cb7114852bc4045c
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-09-16 13:55:16 -0700

    complete spark integration, add styling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,28 +8,24 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
+    staticFileLocation("/public");
     Dictionary newDictionary = new Dictionary();
 
     get("/", (request,response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      Dictionary temp = newDictionary;
       model.put("Dictionary",newDictionary);
       model.put("template","templates/index.vtl");
       return new ModelAndView(model,layout);
     }, new VelocityTemplateEngine());
 
-    // get("/success", (request,response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template","templates/success.vtl");
-    //   return new ModelAndView(model,layout);
-    // }, new VelocityTemplateEngine());
-
-    get("/add-word", (request,response) -> {
+    get("/word/new", (request,response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template","templates/add-word.vtl");
       return new ModelAndView(model,layout);
     }, new VelocityTemplateEngine());
 
-    post("/add-word", (request,response) -> {
+    post("/word/new", (request,response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       String userWord = request.queryParams("word");
       String userDefinition = request.queryParams("definition");
@@ -41,26 +37,21 @@ public class App {
 
     get("/word/:id", (request,response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      Word word = newDictionary.find(Integer.parseInt(request.params(":id")));
       model.put("word",word);
       model.put("template","templates/word.vtl");
       return new ModelAndView(model,layout);
     }, new VelocityTemplateEngine());
 
 
-    get("/word/:id/add-def", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      Word word = Word.find(Integer.parseInt(request.params(":id")));
-      model.put("word",word);
-      model.put("template","templates/add-def.vtl");
-      return new ModelAndView(model,layout);
-    }, new VelocityTemplateEngine());
-
     post("/add-def", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      Definition newDef = new Definition("definition");
-      newDictionary.find(Integer.parseInt(request.queryParams("wordID")));
-      Word word = Word.find(Integer.parseInt(request.params(request.queryParams("wordID"))));
+      String userDef =  request.queryParams("definition");
+      Definition newDef = new Definition(userDef);
+      String temp = request.queryParams("word");
+      Integer temp2 = Integer.parseInt(request.queryParams("word"));
+      newDictionary.find(temp2).addDefinitions(newDef);
+      Word word = newDictionary.find(temp2);
       model.put("word", word);
       model.put("template","templates/word.vtl");
       return new ModelAndView(model,layout);
